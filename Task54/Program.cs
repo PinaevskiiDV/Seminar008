c57af5c101146d973cc1ca1c78b36c32aae9d4ae
Dmitrii <pinochet37@mail.ru>
2022-09-29 11:37:05 +0300

    added Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,54 @@
+﻿int row = new Random().Next(2, 6);
+int column = new Random().Next(2, 6);
+int[,] array = new int[row, column]; 
+
+
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine();
+SortArray(array);
+PrintArray(array);
+
+void CreateArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10); 
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t"); 
+        }
+        Console.WriteLine(); 
+    }
+}
+
+void SortArray(int[,] array)
+{
+    int temp;
+    for (int k = 0; k < array.GetLength(0); k++)
+    {
+        for (int i = 0; i < array.GetLength(1) - 1; i++)
+        {
+            for (int j = i + 1; j < array.GetLength(1); j++)
+            {
+                if (array[k, i] < array[k, j])
+                {
+                    temp = array[k, i];
+                    array[k, i] = array[k, j];
+                    array[k, j] = temp;
+                }
+            }
+        }
+    }
+}
